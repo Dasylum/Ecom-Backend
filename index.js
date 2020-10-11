5e456287cac77b95297527e897ada46ecbf59137
Dasylum <ds19987@gmail.com>
2020-10-11 16:51:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var stripe = require('stripe');
 var geoip = require('geoip-lite');
 var nodemailer = require('nodemailer');
 
+//Account from which the confirmation mail will be sent to the customer
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -33,6 +34,7 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 
 var port = process.env.PORT || 8080;
 
+//Authentication Middleware
 passport.use(
     new LocalStrategy((username, password, done) => {
       customerModel.findOne({ username: username }, (err, user) => {
@@ -69,6 +71,7 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
+//Mongoose databse setup
 mongoose.connect('mongodb+srv://NewDiet:Ds8762402382465@cluster0.sbfkl.mongodb.net/EcomDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 var customerModel = require('./models/customer');
@@ -76,11 +79,12 @@ var productModel = require('./models/product');
 var orderModel = require('./models/order');
 var loginLogsModel = require('./models/loginLogs');
 
+//Login Route
 app.post('/login', passport.authenticate('local'), (req, res) => {
-
     res.json(req.user);
 })
 
+//Log-out Route
 app.get('/log-out', (req, res) => {
     req.logout();
     res.json({
@@ -88,7 +92,8 @@ app.get('/log-out', (req, res) => {
     })
 })
 
-app.post('/add', (req, res) => {
+//Sign-up Route
+app.post('/signup', (req, res) => {
     customerModel.findOne({username: req.body.username}).then(result => {
         if( result ) {
             res.json({
@@ -99,7 +104,7 @@ app.post('/add', (req, res) => {
         else {
             const { firstName, lastName, adminStatus, password, username, email } = req.body;
             const ipAddress = req.ip;
-            var location = geoip.lookup(ipAddress);
+            var location = geoip.lookup(ipAddress);  //Gets the location of the customer 
 
             bcrypt.hash(password, 10, (err, hashedPassword) => {
                 const customerInstance = new customerModel({
@@ -128,24 +133,28 @@ app.post('/add', (req, res) => {
     })
 })
 
+//Daily logins report for the admin to see
 app.get('/reports/logins', (req, res) => {
     var nowDate = new Date(); 
     var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate(); 
     loginLogsModel.find({date: date}).then(result => res.json(result));
 })
 
+//Sales report for the admin to see
 app.get('/reports/sales', (req, res) => {
     var nowDate = new Date(); 
     var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate(); 
     orderModel.find({date: date}).then(result => res.json(result));
 })
 
+//Displays the list of all the buyers/sellers
 app.get('/users', (req, res) => {
     customerModel.find().then(result => {
         res.json(result);
     })
 })
 
+//Adding products
 app.post('/products/add/:id', (req, res) => {
     const {name, price, quantity} = req.body;
     const category = req.params.id;
@@ -170,57 +179,14 @@ app.post('/products/add/:id', (req, res) => {
     })
 })
 
+//Displaying products
 app.get('/products', (req, res) => {
     productModel.find().then(result => {
         res.json(result);
     })
 })
 
-app.post('/order/payment', (req, res) => {
-    customerModel.findById(req.params.customer).then(result => {
-        stripe.customers.create({
-            name: result.firstName,
-            email: result.email,
-            source: req.body.stripeToken
-        })
-        .then(customer =>
-            productModel.findById(req.params.product).then(result => {
-                stripe.charges.create({
-                    amount:  result.price* 100,
-                    currency: "inr",
-                    customer: customer.id
-                  })
-            })
-        )
-        .then(() => {
-            var orderInstance = new orderModel();
-            orderInstance.customer = req.params.customer;
-            orderInstance.product = req.params.product;
-
-            var nowDate = new Date(); 
-            orderInstance.date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate();
-    
-            orderInstance.save().then((err, result) => {
-                var mailOptions = {
-                    from: 'email@gmail.com',
-                    to: req.body.email,
-                    subject: 'Order Confirmed',
-                    text: 'Your order has been placed successfully.'
-                  };
-                  
-                transporter.sendMail(mailOptions, function(error, info){
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log('Email sent: ' + info.response);
-                    }
-                  });
-            })
-        })
-        .catch(err => console.log(err));
-    })
-})
-
+//Route to accept payment and place order 
 app.post("/order/payment/:customer/:product", (req, res) => {
     try {
       stripe.customers
@@ -246,11 +212,22 @@ app.post("/order/payment/:customer/:product", (req, res) => {
 
             var nowDate = new Date(); 
             orderInstance.date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate();
-            
+ 
             orderInstance.save().then((err, result) => {
-                res.json({
-                    message: "Order placed!"
-                })
+                 var mailOptions = {
+                    from: 'email@gmail.com',
+                    to: req.body.email,
+                    subject: 'Order Confirmed',
+                    text: 'Your order has been placed successfully.'
+                  };
+                  
+                transporter.sendMail(mailOptions, function(error, info){       //Sends mail to the customer after the order has been confirmed
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log('Email sent: ' + info.response);
+                    }
+                  });
             })
         })
         .catch(err => console.log(err));
@@ -259,12 +236,14 @@ app.post("/order/payment/:customer/:product", (req, res) => {
     }
 });
 
+//Displays list of all the orders
 app.get('/orders', (req, res) => {
     orderModel.find().then(result => {
         res.json(result);
     })
 })
 
+//Seller can delete a product
 app.delete('/product/delete/:id', (req, res) => {
     productModel.findByIdAndDelete(req.params.id).then(result => {
         res.json({
